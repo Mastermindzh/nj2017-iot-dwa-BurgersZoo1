85e9a416bada0e7798ad82c7fe0e1d40dba61150
Asher <asher.devries@gmail.com>
2017-11-21 16:02:37 +0100

    add material-ui dependency

--- apps/admin/src/components/root.js
+++ apps/admin/src/components/root.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import App from './app';
 
 export default class Root extends Component {
@@ -10,7 +12,9 @@ export default class Root extends Component {
     return (
       <Provider store={store}>
         <ConnectedRouter history={history}>
-          <App />
+          <MuiThemeProvider>
+            <App />
+          </MuiThemeProvider>
         </ConnectedRouter>
       </Provider>
     );
