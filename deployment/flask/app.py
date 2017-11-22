d6890714aab7ad882d8477fec706816f40d36ec5
mastermindzh <info@rickvanlieshout.com>
2017-11-22 14:10:21 +0100

    forgot the background flag :)

--- deployment/flask/app.py
+++ deployment/flask/app.py
@@ -4,24 +4,21 @@ from flask import Flask
 import subprocess
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
     return "Hello World!"
 
 @app.route('/groep1/<repo>')
 def groep1(repo):
-    cmd = ["bash", "deploy.sh" , "groep1", repo]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
-    out, err = p.communicate()
-    return out
+    cmd = ["nohup","bash", "deploy.sh" , "groep1", repo]
+    p = subprocess.Popen(cmd)
+    return ":D"
 
 @app.route('/groep2/<repo>')
 def groep2(repo):
-    cmd = ["bash", "deploy.sh" , "groep2", repo]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
-    out, err = p.communicate()
-    return out
+    cmd = ["nohup", "bash", "deploy.sh" , "groep2", repo]
+    p = subprocess.Popen(cmd)
+    return ":D"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8149)
