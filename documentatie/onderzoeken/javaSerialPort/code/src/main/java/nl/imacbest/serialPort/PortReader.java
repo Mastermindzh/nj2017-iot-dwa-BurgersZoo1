544f8a27e9bb5d2121dca1af784032c7c106d91d
Thomas <thomas.erin999@gmail.com>
2017-11-29 14:43:39 +0100

    Port reader onderzoek

--- documentatie/onderzoeken/javaSerialPort/code/src/main/java/nl/imacbest/serialPort/PortReader.java
+++ documentatie/onderzoeken/javaSerialPort/code/src/main/java/nl/imacbest/serialPort/PortReader.java
@@ -1,23 +1,47 @@
 package nl.imacbest.serialPort;
 
 import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
 
 import java.io.InputStream;
 
 public class PortReader {
+    public static boolean wait = true;
 
     public static void main(String[] args) {
-
         SerialPort comPort = SerialPort.getCommPorts()[0];
         comPort.openPort();
-        comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 100, 0);
-        InputStream in = comPort.getInputStream();
-        try
-        {
-            for (int j = 0; j < 1000; ++j)
-                System.out.print((char)in.read());
-            in.close();
-        } catch (Exception e) { e.printStackTrace(); }
+        comPort.setBaudRate(9600);
+        comPort.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 100, 0);
+        comPort.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent event) {
+                if (event != null) {
+                    wait = false;
+                }
+                System.out.println(new String(event.getReceivedData()));
+            }
+        });
+
+        while (wait) {
+
+        }
+
+
+//        InputStream in = comPort.getInputStream();
+//        try {
+//            for (int j = 0; j < 1000; ++j)
+//                System.out.print((char) in.read());
+//            in.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         comPort.closePort();
     }
 }
