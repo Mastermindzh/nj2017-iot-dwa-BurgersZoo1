18b2cfa72ba2474bb6a50e36bde17d998897c25b
Sharon Franke <sharonfranke94@gmail.com>
2017-12-11 11:16:15 +0100

    .

--- apps/server/server/server.js
+++ apps/server/server/server.js
@@ -4,10 +4,14 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
 
-app.start = function() {
+
+app.start = function () {
   // start the web server
-  return app.listen(function() {
+  return app.listen(function () {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -20,10 +24,51 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
+
+//======================================================================
+  //todo what to do if file name already exists?
+  //toto filetoupload nog steeds....
+  app.post('/upload', function (req, res) {
+
+
+    console.log(req.file)
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = './audio' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+    });
+  })
+
 
   // start the server if `$ node server.js`
   if (require.main === module)
     app.start();
 });
+
+
+/**
+ * Local function that moves the file to a different location on the filesystem
+ * Takes two function arguments to make it compatible w/ Promise or Callback APIs
+ * @param {Function} successFunc
+ * @param {Function} errorFunc
+ */
+function doMove(successFunc, errorFunc) {
+  const fstream = fs.createWriteStream(path);
+
+  streamifier.createReadStream(buf).pipe(fstream);
+
+  fstream.on('error', function (error) {
+    errorFunc(error);
+  });
+
+  fstream.on('close', function () {
+    successFunc();
+  });
+}
