39a2e880b2cc9feaa7c0f66a70a491ec6059c2cf
Asher <asher.devries@gmail.com>
2017-12-13 13:56:34 +0100

    add -h argument for <host>:<port>

--- seedscript.sh
+++ seedscript.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 DB_HOST=localhost:8009
+
+while getopts h: option
+do
+ case "${option}"
+ in
+ h) DB_HOST=${OPTARG};;
+ esac
+done
+
+
 mongorestore --host ${DB_HOST} --db burgerszoo --drop ./assets/seed-data
