b53fad07fb7c0b7f2d7616fa268d36943396dc7b
Asher <asher.devries@gmail.com>
2017-12-13 14:44:03 +0100

    add dir to variable

--- seedscript.sh
+++ seedscript.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 DB_HOST=localhost:8009
+DIR=./assets/seed-data
 
 while getopts h: option
 do
  case "${option}"
  in
- h) DB_HOST=${OPTARG};;
+ h) DB_HOST=${OPTARG};
  esac
 done
 
-
-mongorestore --host ${DB_HOST} --db burgerszoo --drop ./assets/seed-data
+mongorestore --host ${DB_HOST} --nsInclude "burgerszoo.*" --drop ${DIR}
