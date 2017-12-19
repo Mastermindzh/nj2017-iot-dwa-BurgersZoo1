b1bb4ccc9c6c2f8548ed6614af0b1c0354fc9233
Sharon Franke <sharonfranke94@gmail.com>
2017-12-19 13:34:21 +0100

    merge develop

--- apps/admin/src/constants/endpoint-constants.js
+++ apps/admin/src/constants/endpoint-constants.js
@@ -1,6 +1,6 @@
-const SERVER = "http://localhost:8001/api";
-const SPEURPUNTEN = "/speurpunten";
 const SERVERBASE = "http://localhost:8001";
+const SERVER = `${SERVERBASE}/api`;
+const SPEURPUNTEN = "/speurpunten";
 
 export const FILTER = {
   ALL: `?filter=%7B%22include%22%3A%20%5B%22dierengeluid%22%2C%22verblijf%22%2C%20%22weetjes%22%5D%7D`
