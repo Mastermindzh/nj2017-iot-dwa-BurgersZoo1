d5cf9d0dfabde630d61018ade0355338714a229d
Thomas <thomas.erin999@gmail.com>
2018-01-08 12:09:41 +0100

    shaduwJar for building jar

--- apps/gateway/src/main/java/nl/han/Application.java
+++ apps/gateway/src/main/java/nl/han/Application.java
@@ -17,9 +17,9 @@ public class Application {
 
 
     public static void main(String[] args) {
-//
+
         Application application = new Application();
-//
+
         application.setupSpark();
         application.setupSerial();
         application.registerRoutes();
